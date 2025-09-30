bc1276577568f2c0d7f799e5636875960837042c
Negroni1 <negronialanis2@gmail.com>
2025-09-30 17:08:37 -0400

    Set up DB configuration

---
+++ Config/database.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$username   = $_ENV['DB_USER'];
+$password   = $_ENV['DB_PASSWORD'];
+$dbname     = $_ENV['DB_NAME'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    http_response_code(500);
+    die(json_encode(["error" => "DB connection failed: " . $conn->connect_error]));
+}
+
+?>
