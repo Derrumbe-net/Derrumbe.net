c33a4e468b5efeacfc87a7fc7935cb5e991261c6
Negroni1 <negronialanis2@gmail.com>
2025-09-30 17:11:16 -0400

    Refactor .env location

--- Config/database.php
+++ Config/database.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 $servername = $_ENV['DB_HOST'];
